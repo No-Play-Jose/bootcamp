7bfff1bf61a197b81d7af24ae471c569bfecc10d
Kyle <55135527+rankakuhub@users.noreply.github.com>
2019-11-18 19:39:12 +0000

    Update Gun.cs

--- Gun.cs
+++ Gun.cs
@@ -1,5 +1,5 @@
-﻿
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
@@ -8,20 +8,43 @@ public class Gun : MonoBehaviour
     public float fireRate = 15f;
     public float impactForce = 30f;
 
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0f;
     private AudioSource mAudioSrc;
-    // Update is called once per frame
 
-    private void Start()
+    public Animator animator;
+
+    void Start()
     {
+        currentAmmo = maxAmmo;
+
         mAudioSrc = GetComponent<AudioSource>();
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
     }
     void Update()
     {
+
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
@@ -30,18 +53,35 @@ public class Gun : MonoBehaviour
 
             mAudioSrc.Play();
         }
-        
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+
+        animator.SetBool("Reloading", false);
+
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 
     void Shoot()
     {
         muzzleFlash.Play();
 
+        currentAmmo--;
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
 
         {
-            Debug.Log(hit.transform.name);
 
            Target target = hit.transform.GetComponent<Target>();
 
