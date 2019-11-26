6d5ede205a1440f36981202442977d5cfc1d090a
Kyle <55135527+rankakuhub@users.noreply.github.com>
2019-11-26 10:20:15 +0000

    Scripts
    Scripts for Bootcamp

---
+++ Scripts/Gun/Target.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            SumScore.Add( pointsToAdd: 1);
+            Die();
+            
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Scripts/PauseMenu.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    private AudioSource mAudioSrc;
+ 
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+                Cursor.visible = false;
+
+            }
+            else
+            {
+                Pause();
+                Cursor.visible = true;
+            }
+        }
+    }
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        AudioListener.volume = 1;
+        Cursor.visible = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        AudioListener.volume = 0;
+        Cursor.visible = true;
+
+    }
+    
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("Menu");
+        Cursor.visible = true;
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting game...");
+        Application.Quit();
+    }
+}
